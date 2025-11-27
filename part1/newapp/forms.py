11a005b6e33d28d3f2cccaa395ac8d9894326edf
r-on-s <ronyarif700@gmail.com>
2025-11-27 08:07:48 +0600

    built in form part1

--- part1/newapp/forms.py
+++ part1/newapp/forms.py
@@ -2,4 +2,9 @@ from django import forms
 class SampleForm(forms.Form):
     name = forms.CharField(label='User Name')
     email = forms.EmailField(label='User Email')
-   
+    age = forms.IntegerField(label='User Age')
+    weight = forms.FloatField(label='User Weight')
+    balance = forms.DecimalField(label='Account Balance')
+    check=forms.BooleanField(label='Accept Terms')
+    birth_date = forms.DateField(label='Birth Date')   
+    appointment_time = forms.DateTimeField(label='Appointment Time')
