073eeb961689aad9d05d6728a35bc2248c02a122
r-on-s <ronyarif700@gmail.com>
2025-12-11 17:08:28 +0600

    delete a item

--- part1/newapp1/views.py
+++ part1/newapp1/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from . import models
 
@@ -7,3 +7,7 @@ def mode(request):
     students=models.SampleModel.objects.all()
     print(students)
     return render(request,'new.html',{'data':students})
+def delete_data(request,roll):
+    std=models.SampleModel.objects.get(roll=roll).delete()
+    print(std)
+    return redirect('mode')
